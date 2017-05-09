c1ddb2ede3636e64037efac7b4ca7602b90502b7
Tommie McAfee <tahmmee@gmail.com>
2017-05-09 11:34:54 -0400

    parameterize the docker address

--- scripts/session.sh
+++ scripts/session.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+CLIENT=$1 
 DEFAULT_SESSION=$(openssl rand -hex 12)
 EX_ARGS=""
 SESSION_ARG=`echo $* | grep "session:"  |  sed 's/.*session://' |  sed 's/-/_tty_/'`
@@ -12,4 +12,4 @@ SESSION=${SESSION_ARG:-$DEFAULT_SESSION}
 
 echo $SESSION > /tmp/$$
 tmux detach -s $SESSION 2> /dev/null || true
-tmux new -A -s $SESSION docker run --name $SESSION -it toastty 52.207.212.163:2375  $*" $EX_ARGS"
+tmux new -A -s $SESSION docker run --name $SESSION -it toastty $CLIENT  $*" $EX_ARGS"
